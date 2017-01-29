c5bad4050e59e2d6a0f5b46da88b0e724f37f18c
Tihomir Nedev <hartrox@gmail.com>
2017-01-29 12:48:30 +0200

    solution

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
-# behavioral_cloning
+# Behavioral Cloning
+
+This project is part of Udacity's self-driving cars nanodegree.
+The task is to design a deep neural network to clone the human driving behavior.
+To make testing and obtaining data easier, Udacity made a simulator, however, 
+the data and techniques used are no different for real world application
+
+The objective is to design a steering model for the car. Driving image comes in, steering angle comes out.
+If successful the simulated car will be able to drive autonomously on the provided track.
+We are also able to control the throttle of the car, but I decided that for this case, controlling the throttle is pointless. 
+
+The simulator could provide front, left and right images with 320x160 images. 
+I decided to only use the central(front) images, because later I want to test the model with a real life data, and obtaining single single camera data will bi a lot easier. 
+
+
+I've spent days into experimenting and training with different models. 
+
+First, decided to start with the Comma.ai steering model.
+Found here (https://github.com/commaai/research/blob/master/train_steering_model.py)
+
+I've started with a plain model weights instead of obtaining the weights of their training. 
+After many iterations of obtaining data (driving the simulator), preprocessing and training, I could not make
+this model behave well. I managed to go through half the track autonomously, but is seems like the model is too complex for
+the data and without and initial training of transfer learning I won't be able to make it work. 
+
+Second, I made my own model with 2 conv layers and 3 fully connected layers. Again, after many tries for collecting data and training
+I could not make the model generalize well. 
+
+My main confusion was with simultaneously collecting the data and testing my model. I did not know if the 
+data I collected was enough, was it balanced well or was the problem in the model. This is a very vicious cycle of 
+data collection and preprocessing and model changes. With this complexity of options is hard to decide on a model and data.
+
+Third, I completely switched gears and decided to go minimalistic. I preprocessed all images to grayscale and 32x16, as I read
+some people manages it run with it. My model stayed the same as previous one. I started completely fresh data minimalistic style. 
+Did only two laps and then two more laps of data where the car goes off track and I turn it back to the center. Immediately saw improvement inn driving behavior, 
+and the simulator car was able to drive well on 3/4 of track. The last two corners were hard. I started collecting more and more data just on those corners, 
+but did not see improvement and even the opposite happened. After doubling the data, the car started behaving worse. 
+I really liked this minimalistic style but it seemed like the car was not able to generalize. It drives reasonably well, but
+it looked like it remembers rather than understands. I saw that this model could be made to fully work, but I was already
+tired from experimenting without good results. Decided to move forward and try another approach.
+
+Fourth and final approach was to test with transfer learning. 
+I decided to go with the VGG model and Imagenet weights. Removed the last block of the VGG and added my layers on top. 
+I knew I needed more data to make this work. Therefore, I also decided to flip each image and angle which will double every data point.
+Even after my first training the car behaved very differently it seemed that it was able to generalize and knew that it should
+not go over the marked lines and offtrack. Because the model is complex and images are quite big it takes a while to train. 
+I used my Azure GPU VM with 12 CPU cores and 2xTesla K80 GPUs. With it training with 80k data points with Python generators it takes about
+2 hours. Most of this time comes from using generators instead of in memory loading all the data.
+After a few iterations and improvements and training cycles of the model I was able to successfully make it through the first track.
+
+Funny enough, this model drives very well, but it is unable to run on my laptop in real time due to its complexity.
+To be able to test I run the simulator on my laptop while the model runs on the Azure VM, connecting them though a SSH tunneling.
+However, even that my network connection is good there is a significant lag maybe because of the nature of the ssh connection
+and that it has to send 10 images a second. Therefore, I was only able to run in autonomous mode in 0.15 throttle.
+Every more throttle and the lag makes the car goes offroad.
+
+Another problem I found is with the simulator. In the simulator the car could easily get stuck on the sided of the road.
+It's a pity, because otherwise we would be able to see the car completely off and the get back to the track. It did not get stuck with also
+made sense for the model to control the throttle and being able to go back. However, due to those limitations, we only need to drive on track all the time.
+
+Overall, I did days of training and experimenting to come with a good model, but in the process I learned a lot. 
+The task is hard. I could spend further weeks of experimenting in order to come with a better model and optimum training data.
+I am happy with the results and I will stop here with the simulator, however, I will continue with real life data.
+I will first use the data provided by Comma.ai and then try collecting my own data by producing a simple device to do so.
+
+Fun times ahead!
--- model.py
+++ model.py
@@ -1,30 +1,44 @@
 import csv
 import json
-
+import os.path
+from keras.applications import VGG16
+from keras.engine import Model
 from keras.layers import Input, BatchNormalization, Convolution2D, Dense, Flatten, MaxPooling2D, ELU, Dropout, \
-    Convolution1D, Activation, MaxPooling1D
-from keras.models import Sequential
+    Convolution1D, Activation, MaxPooling1D, Lambda, Conv1D
+from keras.models import Sequential, model_from_json
 from keras.optimizers import Adam
+from pathlib import Path
 from scipy.misc import imread, imresize, imsave
 import numpy as np
-import pickle
 from sklearn.utils import shuffle
 
 
-image_shape = (16*2, 32*2)
+image_shape = (160, 320, 3)
+
 def normalize_grayscale(image_data):
-    a = -0.5
-    b = 0.5
+    a = -1
+    b = 1
     grayscale_min = 0
     grayscale_max = 255
     return a + ( ( (image_data - grayscale_min)*(b - a) )/( grayscale_max - grayscale_min ) )
 
 
-def load_image(image_path):
-    image = imread(image_path, flatten=True)
-    image = imresize(image, size=image_shape)
+def load_image(image_path, reverse=False):
+    image = imread(image_path)
+    if reverse:
+        image = np.fliplr(image)
+    image = preprocess_image(image)
+    return image
+
+
+def preprocess_image(image):
+
+    # image = image[60::]
+    # image = imresize(image, size=image_shape)
     image = normalize_grayscale(image)
-    return np.array([image])
+    image = np.array([image])
+
+    return image
 
 
 def read_data(data):
@@ -33,7 +47,11 @@ def read_data(data):
         i += 1
         if i == len(data):
             i = 0
-        yield load_image(data[i][0]), np.array([data[i][1]])
+        reverse = data[i][2]
+        if reverse:
+            yield load_image(data[i][0], reverse), np.array([-float(data[i][1])])
+        else:
+            yield load_image(data[i][0]), np.array([float(data[i][1])])
 
 
 def read_data_file(file_path):
@@ -41,37 +59,143 @@ def read_data_file(file_path):
     with open(file_path, 'r') as csvfile:
         reader = csv.reader(csvfile, delimiter=',', quotechar='|')
         for row in reader:
-            data.append([row[0], row[3]])
+            if os.path.exists(row[0]):
+                data.append([row[0], row[3], False])
+                data.append([row[0], row[3], True])
+            else:
+                print(row[0])
     return data
 
 
-data = read_data_file('driving_log.csv')
-data = shuffle(data)
-# data = data[:int(len(data)/2)]
-validation_index = int(len(data)*0.2)
-test_index = validation_index + int(len(data)*0.1)
-validation_data = data[0:validation_index]
-test_data = data[validation_index:test_index]
-train_data = data[test_index:]
+def get_vgg_based_model():
 
-model = Sequential()
-model.add(Convolution1D(16, 3, border_mode="same", input_shape=image_shape))
-model.add(Activation('relu'))
-model.add(Convolution1D(32, 3, border_mode="same"))
-model.add(Flatten())
-model.add(Activation('relu'))
-model.add(Dense(64))
-model.add(Dropout(.5))
-model.add(Activation('relu'))
-model.add(Dense(1))
+    base_model = VGG16(weights='imagenet', include_top=False, input_shape=image_shape)
 
-adam = Adam(0.0001)
-model.compile(optimizer=adam, loss="mse")
-history = model.fit_generator(read_data(train_data), validation_data=read_data(validation_data),
-                              samples_per_epoch=len(train_data), nb_epoch=10, nb_val_samples=len(validation_data))
-test_lost = model.evaluate_generator(read_data(test_data), len(test_data))
-print("Test lost: ", test_lost)
-model_json = model.to_json()
-with open('model.json', 'w') as outfile:
-    outfile.write(model_json)
-model.save_weights('model.h5')
+    # Remove the last block of layers in the VGG
+    [base_model.layers.pop() for _ in range(4)]
+    base_model.outputs = [base_model.layers[-1].output]
+    base_model.layers[-1].outbound_nodes = []
+
+    # Because the data in VGG model is different from what our task is, we disable the training on the base model
+    for layer in base_model.layers:
+        layer.trainable = False
+
+    layer = base_model.outputs[0]
+    layer = Convolution2D(512, 3, 3, subsample=(2, 2), activation='relu', border_mode='same')(
+        layer)
+    layer = Convolution2D(512, 3, 3, subsample=(1, 2), activation='relu', border_mode='same')(
+        layer)
+    layer = Convolution2D(512, 3, 3, subsample=(1, 2), activation='relu', border_mode='same')(
+        layer)
+
+    layer = Flatten()(layer)
+    layer = Dropout(.2)(layer)
+    layer = Dense(2048, activation='relu')(layer)
+    layer = Dropout(.2)(layer)
+    layer = Dense(1024, activation='relu')(layer)
+    layer = Dropout(.5)(layer)
+    layer = Dense(1, activation='tanh')(layer)
+
+    model = Model(input=base_model.input, output=layer)
+
+    adam = Adam(0.00001)
+    model.compile(optimizer=adam, loss="mse")
+
+    return model
+
+
+def get_comma_model():
+
+    ch, row, col = 3, 160, 320  # camera format
+    model = Sequential()
+    model.add(Convolution2D(16, 8, 8, subsample=(4, 4), border_mode="same", input_shape=image_shape))
+    model.add(ELU())
+    model.add(Convolution2D(32, 5, 5, subsample=(2, 2), border_mode="same"))
+    model.add(ELU())
+    model.add(Convolution2D(64, 5, 5, subsample=(2, 2), border_mode="same"))
+    model.add(Flatten())
+    model.add(Dropout(.2))
+    model.add(ELU())
+    model.add(Dense(512))
+    model.add(Dropout(.5))
+    model.add(ELU())
+    model.add(Dense(1))
+
+    model.compile(optimizer="adam", loss="mse")
+
+    return model
+
+
+def get_simple_model():
+
+    model = Sequential()
+    model.add(Conv1D(32, 3, input_shape=image_shape, border_mode='same', activation='relu'))
+    model.add(Conv1D(64, 3, border_mode='same', activation='relu'))
+    model.add(Conv1D(128, 3, border_mode='same', activation='relu'))
+    model.add(Conv1D(256, 3, border_mode='same', activation='relu'))
+    model.add(Flatten())
+    model.add(Dense(1024, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(512, activation='relu'))
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(1, activation='tanh'))
+
+    model.compile(optimizer=Adam(lr=0.00001), loss='mse')
+
+    return model
+
+
+def load_data(shuffle_data=True):
+
+    data = []
+    data += read_data_file('driving_log.csv')
+
+    if shuffle_data:
+        data = shuffle(data)
+
+    return data
+
+
+def split_data(data):
+    """
+    Split the data in train/validation
+    Test set is missed because it did not show valuable information during the training experimentation
+
+    :param data:
+    :return:
+    """
+    validation_index = int(len(data) * 0.1)
+    validation_data = data[0:validation_index]
+    train_data = data[validation_index:]
+
+    return train_data, validation_data
+
+
+def train_generator(model, train_data, validation_data, continuation_learning = False, epoch=4):
+
+    if continuation_learning:
+        model.load_weights('model.h5')
+
+    history = model.fit_generator(read_data(train_data), validation_data=read_data(validation_data),
+                                  samples_per_epoch=len(train_data), nb_epoch=4, nb_val_samples=len(validation_data),
+                                  nb_worker=1)
+    return history
+
+
+def save_model_and_weights(model):
+
+    model_json = model.to_json()
+    with open('model.json', 'w') as outfile:
+        outfile.write(model_json)
+    model.save_weights('model.h5', overwrite=True)
+
+if __name__ == '__main__':
+
+    data = load_data()
+    train_data, validation_data = split_data(data)
+
+    model = get_vgg_based_model()
+
+    history = train_generator(model, train_data, validation_data)
+
+    save_model_and_weights(model)
